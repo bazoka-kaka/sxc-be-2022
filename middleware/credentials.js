b8b854b5c29d95a898170c4f7622c60b7f8ceb70
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-06-06 18:54:59 +0700

    fix credentials component setHeader

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,11 +1,20 @@
-const allowedOrigins = require('../config/allowedOrigins');
+const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Credentials', true);
-    }
-    next();
-}
+  const origin = req.headers.origin;
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+  }
+  next();
+};
 
-module.exports = credentials
+module.exports = credentials;
