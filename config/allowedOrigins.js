3f2fe1e100d9f9f5e32d8f362c7f46a12feb41a6
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-06-06 18:05:36 +0700

    add allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-    'https://sxcleague2022.vercel.app/',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000'
+  "https://sxcleague2022.vercel.app/",
+  "https://sxcleague2022-nl75sftx6-julyo11.vercel.app/",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "http://localhost:3000",
 ];
 
 module.exports = allowedOrigins;
