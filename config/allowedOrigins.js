14150847cd80daaa8a02fa43f292db9c3bc254a7
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-06-07 11:17:43 +0700

    update allowedOrigins origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
+  "https://sxc-be-2022.herokuapp.com/",
 ];
 
 module.exports = allowedOrigins;
