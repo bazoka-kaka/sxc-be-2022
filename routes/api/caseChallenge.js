4952ef797aa96d69c39825357307731acbab86d8
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-06-14 01:04:34 +0700

    add protected routes on caseChallenge route

--- routes/api/caseChallenge.js
+++ routes/api/caseChallenge.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const caseChallengeController = require("../../controllers/caseChallengeController");
+const ROLES_LIST = require("../../config/roles_list");
+const verifyRoles = require("../../middleware/verifyRoles");
 
 router
   .route("/")
-  .get(caseChallengeController.getAllTeams)
-  .post(caseChallengeController.createNewTeam)
-  .put(caseChallengeController.updateTeam)
-  .delete(caseChallengeController.deleteTeam);
+  .get(verifyRoles(ROLES_LIST.Admin), caseChallengeController.getAllTeams)
+  .post(
+    verifyRoles(ROLES_LIST.User, ROLES_LIST.Admin),
+    caseChallengeController.createNewTeam
+  )
+  .put(verifyRoles(ROLES_LIST.Admin), caseChallengeController.updateTeam)
+  .delete(verifyRoles(ROLES_LIST.Admin), caseChallengeController.deleteTeam);
 
 router.route("/:id").get(caseChallengeController.getTeam);
 
