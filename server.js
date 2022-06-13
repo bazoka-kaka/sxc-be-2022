e6e8b154dfc6bc19395a460e95424620d02d754d
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-06-14 01:53:36 +0700

    unprotect casechallenge route

--- server.js
+++ server.js
@@ -45,9 +45,9 @@ app.use("/auth", require("./routes/auth"));
 app.use("/refresh", require("./routes/refresh"));
 app.use("/logout", require("./routes/logout"));
 
+app.use("/case-challenge", require("./routes/api/caseChallenge"));
 app.use(verifyJWT);
 app.use("/employees", require("./routes/api/employees"));
-app.use("/case-challenge", require("./routes/api/caseChallenge"));
 app.use("/users", require("./routes/api/users"));
 
 app.all("*", (req, res) => {
