d980ab07a71aa04ebf6822e3c7d0eb0f58673f38
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-06-14 10:57:17 +0700

    remove caseChallenge from protected routes

--- routes/api/caseChallenge.js
+++ routes/api/caseChallenge.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const caseChallengeController = require("../../controllers/caseChallengeController");
-const ROLES_LIST = require("../../config/roles_list");
-const verifyRoles = require("../../middleware/verifyRoles");
 
 router
   .route("/")
-  .get(verifyRoles(ROLES_LIST.Admin), caseChallengeController.getAllTeams)
-  .post(
-    verifyRoles(ROLES_LIST.User, ROLES_LIST.Admin),
-    caseChallengeController.createNewTeam
-  )
-  .put(verifyRoles(ROLES_LIST.Admin), caseChallengeController.updateTeam)
-  .delete(verifyRoles(ROLES_LIST.Admin), caseChallengeController.deleteTeam);
+  .get(caseChallengeController.getAllTeams)
+  .post(caseChallengeController.createNewTeam)
+  .put(caseChallengeController.updateTeam)
+  .delete(caseChallengeController.deleteTeam);
 
 router.route("/:id").get(caseChallengeController.getTeam);
 
